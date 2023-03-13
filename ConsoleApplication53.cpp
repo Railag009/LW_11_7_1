f7c63a39a39a28a8ca69b37eee8c748fc535d8f4
Кирилл Соловьёв <Solovevkirill26072002@ya.ru>
2023-03-13 14:08:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication53.cpp
@@ -0,0 +1,72 @@
+﻿#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct Dlist
+{
+
+	int info;
+	Dlist* next, * prev;
+
+};
+
+
+int main()
+{
+
+	Dlist* head, * p, * r;
+	int x, k, y;
+	int max, i, a, b;
+	float sr;
+
+	cout << "Enter a - ";
+	cin >> a;
+
+	cout << "Enter b - ";
+	cin >> b;
+
+	cin >> x;
+	head = new Dlist;
+	head->info = x;
+	p = head;
+
+
+
+	k = 0;
+
+	cin >> x;
+	while (x)
+	{
+		r = new Dlist;
+		r->info = x;
+		r->prev = p;
+		p->next = r;
+		p = r;
+		cin >> x;
+
+	}
+
+	head->prev = p;
+	p->next = head;
+
+	p = head;
+
+	do
+	{
+
+		if ((p->info >= a) && (p->info <= b))
+		{
+
+			k += p->info;
+
+		}
+
+		p = p->next;
+
+	} while (p != head);
+
+	cout << k;
+
+}
